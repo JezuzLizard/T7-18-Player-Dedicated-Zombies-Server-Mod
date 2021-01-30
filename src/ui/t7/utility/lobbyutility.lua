6cbefe2e39abadd223f78f8b8c3e8dac0b32e138
JezuzLizard <cycadnano@hotmail.com>
2021-01-30 09:46:45 -0800

    Removed debug code.

--- src/ui/t7/utility/lobbyutility.lua
+++ src/ui/t7/utility/lobbyutility.lua
@@ -6,20 +6,5 @@ function Engine.GetLobbyMaxClients()
       Engine.SetLobbyMaxClients(Enum.LobbyType.LOBBY_TYPE_GAME, 18)
       Engine.SetLobbyMaxClients(Enum.LobbyType.LOBBY_TYPE_PRIVATE, 18)
       Engine.SetLobbyMaxClients(Enum.LobbyType.LOBBY_TYPE_CUSTOM, 18)
-      -- Engine.PrintInfo( Enum.consoleLabel.LABEL_DEFAULT, ( "Max players set to 18." ) )
-      -- Engine.PrintWarning( Enum.consoleLabel.LABEL_DEFAULT, ( "Max players set to 18." ) )
-      -- Dvar.ui_lobbyDebugMsgLevel:set( 2 )
-      -- Dvar.ui_lobbyDebugVis:set( 1 )
-      -- DebugPrint( "Max players set to 18." )
-      -- {}.ShowMessageDialog( "ree", Enum.MessageDialogType.MESSAGE_DIALOG_TYPE_INFO, "Max players set to 18", "big meme" )
-      -- this function works but the error will prevent you from going inside the main menu as well not printing out the message
-      -- error( "Max players set to 18." )
-      -- Engine.PrintError( "Max players set to 18." )
-
-      -- will let you print custom messages but not custom variables
-      -- mode = Engine.GetLobbyMainMode()
-      -- " mode.lobbyType: " .. mode.lobbyType .. " mode.lobbyMode: " .. mode.lobbyMode .. " mode.eGameModes: " .. mode.eGameModes .. "."
-      -- Lobby.Actions.ErrorPopupMsg( "Max players set to 18." )
-      -- Engine.SwitchMode( 0, "zm")
       return 18
 end
